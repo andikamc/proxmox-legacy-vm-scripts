c30c39137d94d08896a19645f80c484307cef1e6
Andika Muhammad Cahya <andkmc99@gmail.com>
2025-03-07 11:14:06 +0700

    Refactor VM disk expansion process in Debian 10 script to use virt-resize for improved efficiency

--- scripts/debian10-vm.sh
+++ scripts/debian10-vm.sh
@@ -487,10 +487,13 @@ apt-get -qq update && apt-get -qq install libguestfs-tools lsb-release -y >/dev/
 msg_ok "Installed libguestfs-tools successfully"
 
 msg_info "Expand VM Disk using parted (/dev/sda1)"
-virt-customize -q -a "${FILE}" --install parted >/dev/null
-virt-customize -q -a "${FILE}" --run-command "parted /dev/sda resizepart 1 100%" >/dev/null
-virt-customize -q -a "${FILE}" --run-command "resize2fs /dev/sda1" >/dev/null
+virt-resize --expand /dev/sda1 ${FILE} ${FILE}-expanded
+mv ${FILE}-expanded ${FILE}
 virt-customize -q -a "${FILE}" --run-command "partprobe" >/dev/null
+# virt-customize -q -a "${FILE}" --install parted >/dev/null
+# virt-customize -q -a "${FILE}" --run-command "parted /dev/sda resizepart 1 100%" >/dev/null
+# virt-customize -q -a "${FILE}" --run-command "resize2fs /dev/sda1" >/dev/null
+# virt-customize -q -a "${FILE}" --run-command "partprobe" >/dev/null
 msg_ok "Expanded VM Disk using parted successfully"
 
 if [ "$START_VM" == "yes" ]; then
