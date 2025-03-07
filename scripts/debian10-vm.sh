32b4132de3b31de30560d156412f60133bf9ac44
Andika Muhammad Cahya <andkmc99@gmail.com>
2025-03-07 14:05:51 +0700

    Update Debian VM scripts to use HTTPS for disk image URLs and remove disk expansion commands

--- scripts/debian10-vm.sh
+++ scripts/debian10-vm.sh
@@ -422,7 +422,7 @@ fi
 msg_ok "Using ${CL}${BL}$STORAGE${CL} ${GN}for Storage Location."
 msg_ok "Virtual Machine ID is ${CL}${BL}$VMID${CL}."
 msg_info "Retrieving the URL for the Debian 10 Qcow2 Disk Image"
-URL=http://cloud.debian.org/images/cloud/buster/20240703-1797/debian-10-nocloud-amd64-20240703-1797.qcow2
+URL=https://mirror.sjtu.edu.cn/debian-cdimage/cloud/buster/20240703-1797/debian-10-nocloud-amd64-20240703-1797.qcow2
 sleep 2
 msg_ok "${CL}${BL}${URL}${CL}"
 wget -q --show-progress $URL
@@ -450,16 +450,7 @@ for i in {0,1}; do
   eval DISK${i}=vm-${VMID}-disk-${i}${DISK_EXT:-}
   eval DISK${i}_REF=${STORAGE}:${DISK_REF:-}${!disk}
 done
-
-msg_info "Expand VM Disk using parted (/dev/sda)"
-virt-customize -q -a "${FILE}" --install qemu-guest-agent,apt-transport-https,ca-certificates,curl,wget,gnupg,software-properties-common,lsb-release,parted >/dev/null &&
-virt-customize -q -a "${FILE}" --run-command 'wget https://github.com/andikamc/proxmox-legacy-vm-scripts/raw/master/services/rc-parted.service -O /lib/systemd/system/rc-parted.service' >/dev/null &&
-virt-customize -q -a "${FILE}" --run-command 'systemctl enable rc-parted' >/dev/null &&
-virt-customize -q -a "${FILE}" --run-command 'wget https://github.com/andikamc/proxmox-legacy-vm-scripts/raw/master/services/rc-parted.sh -O /etc/rc-parted.sh' >/dev/null &&
-virt-customize -q -a "${FILE}" --run-command 'chmod +x /etc/rc-parted.sh' >/dev/null &&
-virt-customize -q -a "${FILE}" --run-command 'echo -n > /etc/machine-id' >/dev/null
-msg_ok "Expanded VM Disk using parted successfully"
-
+virt-customize -q -a "${FILE}" --install qemu-guest-agent,apt-transport-https,ca-certificates,curl,wget,gnupg,software-properties-common,lsb-release,parted >/dev/null
 
 msg_info "Creating a Debian 10 VM"
 qm create $VMID \
